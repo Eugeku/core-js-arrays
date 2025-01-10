e6bd0d6dc76cd7cd426737a64f0bc7a824aa254b
Eugeku <eugenekuzora@gmail.com>
2025-01-10 17:28:33 +0300

    feat: impl removeFalse, findOcurrnces

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -81,8 +81,10 @@ function findElement(arr, value) {
  *    findAllOccurrences([ null, undefined, null ], null) => 2
  *    findAllOccurrences([ true, 0, 1, 'true' ], true) => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  return arr.reduce((accumulator, currentValue) => {
+    return currentValue === item ? accumulator + 1 : accumulator;
+  }, 0);
 }
 
 /**
@@ -97,8 +99,10 @@ function findAllOccurrences(/* arr, item */) {
  *    removeFalsyValues([ 1, 2, 3, 4, 5, 'false' ]) => [ 1, 2, 3, 4, 5, 'false' ]
  *    removeFalsyValues([ false, 0, NaN, '', undefined ]) => [ ]
  */
-function removeFalsyValues(/* arr */) {
-  throw new Error('Not implemented');
+function removeFalsyValues(arr) {
+  return arr.filter((element) => {
+    return element;
+  });
 }
 
 /**
